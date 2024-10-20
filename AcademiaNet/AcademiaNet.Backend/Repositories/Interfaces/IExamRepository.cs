c96ae8ec37339a8db94203d9859c0d50c2c4cbd9
alejandrosalgar <alejandro.salgar@gmail.com>
2024-10-20 15:54:30 -0500

    update exam

---
+++ AcademiaNet/AcademiaNet.Backend/Repositories/Interfaces/IExamRepository.cs
@@ -0,0 +1,11 @@
+﻿using AcademiaNet.Shared.Entites;
+using AcademiaNet.Shared.Responses;
+
+namespace AcademiaNet.Backend.Repositories.Interfaces;
+
+public class IExamRepository
+{
+    Task<ActionResponse<Exam>> GetAsync(int id);
+    Task<ActionResponse<IEnumerable<Exam>>> GetAsync();
+    Task<IEnumerable<Exam>> GetComboAsync();
+}
