c166525ed8399aeb54385c05875fe7e41f1e0713
JuanCarrasquilla360 <juancarrasquilla135219@hotmail.com>
2024-10-20 17:55:04 -0500

    add new modules

---
+++ AcademiaNet/AcademiaNet.Backend/UnitsOfWork/Interfaces/IExamResultUnitOfWork.cs
@@ -0,0 +1,62 @@
+﻿using AcademiaNet.Shared.DTOs;
+using AcademiaNet.Shared.Entites;
+using Microsoft.AspNetCore.Identity;
+
+namespace AcademiaNet.Backend.UnitsOfWork.Interfaces;
+
+public interface IExamResultUnitOfWork
+{
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    Task<string> GeneratePasswordResetTokenAsync(User user);
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="user"></param>
+    /// <param name="token"></param>
+    /// <param name="password"></param>
+    /// <returns></returns>
+
+    Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="user"></param>
+    /// <param name="currentPassword"></param>
+    /// <param name="newPassword"></param>
+    /// <returns></returns>
+    Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+
+    Task<IdentityResult> UpdateUserAsync(User user);
+
+    Task<User> GetUserAsync(Guid userId);
+
+    Task<string> GenerateEmailConfirmationTokenAsync(User user);
+
+    Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
+    Task<SignInResult> LoginAsync(LoginDTO model);
+
+    Task LogoutAsync();
+
+    Task<User> GetUserAsync(string email);
+
+    Task<IdentityResult> AddUserAsync(User user, string password);
+
+    Task CheckRoleAsync(string roleName);
+
+    Task AddUserToRoleAsync(User user, string roleName);
+
+    Task<bool> IsUserInRoleAsync(User user, string roleName);
+}
---
+++ AcademiaNet/AcademiaNet.Shared/DTOs/ExamResultsProgramDTO.cs
@@ -0,0 +1,30 @@
+﻿using AcademiaNet.Shared.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ExamResultsProgramDTO
+{
+    [Key]
+    public int AcademicProgramID { get; set; }
+
+    [MaxLength(100)]
+    [Required]
+    public string Name { get; set; } = null!;
+
+    [MaxLength(100)]
+    [Required]
+    public string Category { get; set; } = null!;
+
+    [Required]
+    public int InstitutionID { get; set; }
+
+    public Institution? Institution { get; set; }
+    public ICollection<Enrollment>? Enrollments { get; set; }
+    public int EnrollmentsCount => Enrollments == null ? 0 : Enrollments.Count;
+    public ICollection<PeriodAcademicProgram>? PeriodAcademicProgramss { get; set; }
+    public int PeriodAcademicProgramssCount => PeriodAcademicProgramss == null ? 0 : PeriodAcademicProgramss.Count;
+}
