265fc8261a58dfc752b1b2701fdeb8269358ea86
alejandrosalgar <alejandro.salgar@gmail.com>
2024-11-28 14:00:32 -0500

    feat: add enrollment service

--- AcademiaNet/AcademiaNet.Backend/Controllers/Enrollments.cs
+++ AcademiaNet/AcademiaNet.Backend/Controllers/Enrollments.cs
@@ -1,5 +1,30 @@
-﻿namespace AcademiaNet.Backend.Controllers;
-public class Enrollments
+﻿using AcademiaNet.Backend.UnitsOfWork.implementations;
+using AcademiaNet.Backend.UnitsOfWork.Interfaces;
+using AcademiaNet.Shared.Entites;
+using Fantasy.Backend.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademiaNet.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class Enrollments: GenericController<Enrollment>
 {
+    private readonly IEnrollmentUnitOfWork _EnrollmentUnitOfWork;
+    public Enrollments(IGenericUnitOfWork<Enrollment> unitOfWork, IEnrollmentUnitOfWork EnrollmentUnitOfWork) : base(unitOfWork)
+    {
+        _EnrollmentUnitOfWork = EnrollmentUnitOfWork;
+    }
+
+    [HttpGet("{id}")]
+    public override async Task<IActionResult> GetAsync(int Id)
+    {
+        var response = await _EnrollmentUnitOfWork.GetAsync(Id);
+        if (response.WasSuccess)
+        {
+            return Ok(response.Result);
+        }
+        return NotFound(response.Message);
+    }
 }
 
--- AcademiaNet/AcademiaNet.Backend/UnitsOfWork/implementations/EnrollmentUnitOfWork.cs
+++ AcademiaNet/AcademiaNet.Backend/UnitsOfWork/implementations/EnrollmentUnitOfWork.cs
@@ -1,6 +1,23 @@
-﻿namespace AcademiaNet.Backend.UnitsOfWork.implementations
+﻿using AcademiaNet.Backend.Repositories.Interfaces;
+using AcademiaNet.Backend.UnitsOfWork.Interfaces;
+using AcademiaNet.Shared.Entites;
+using AcademiaNet.Shared.Responses;
+
+namespace AcademiaNet.Backend.UnitsOfWork.implementations;
+public class EnrollmentUnitOfWork: GenericUnitOfWork<Enrollment>, IEnrollmentUnitOfWork
 {
-    public class EnrollmentUnitOfWork
+    private readonly IEnrollmentRepository _EnrollmentRepository;
+
+    public EnrollmentUnitOfWork(IGenericRepository<Enrollment> repository, IEnrollmentRepository EnrollmentRepository) : base(repository)
     {
+        _EnrollmentRepository = EnrollmentRepository;
     }
+
+    public async Task<Enrollment> AddEnrollmentAsync(Enrollment enrollment) => await _EnrollmentRepository.AddEnrollmentAsync(enrollment);
+    public override async Task<ActionResponse<IEnumerable<Enrollment>>> GetAsync() => await _EnrollmentRepository.GetAsync();
+
+    public override async Task<ActionResponse<Enrollment>> GetAsync(int id) => await _EnrollmentRepository.GetAsync(id);
+
+    public async Task<IEnumerable<Enrollment>> GetComboAsync() => await _EnrollmentRepository.GetComboAsync();
 }
+
